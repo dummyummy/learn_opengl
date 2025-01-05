960e0b1f27c5f3156e94ea7686c4db37492d5002
dummyummy <3384447470@qq.com>
2025-01-05 19:14:41 +0800

    Indexed Drawing

--- main.cpp
+++ main.cpp
@@ -103,26 +103,40 @@ int main()
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f, // left  
-         0.5f, -0.5f, 0.0f, // right 
-         0.0f,  0.5f, 0.0f  // top   
-    }; 
+        0.5f, 0.5f, 0.0f,   // 右上角
+        0.5f, -0.5f, 0.0f,  // 右下角
+        -0.5f, -0.5f, 0.0f, // 左下角
+        -0.5f, 0.5f, 0.0f   // 左上角
+    };
 
-    unsigned int VBO, VAO;
+    unsigned int indices[] = {
+        0, 1, 3, // 第一个三角形
+        1, 2, 3  // 第二个三角形
+    };
+
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     // enable the first attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    glBindBuffer(GL_ARRAY_BUFFER, 0); // unbind VBO
-
     glBindVertexArray(0); // unbind VAO
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0); // unbind VBO
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); // unbind VBO
+
+    // draw in wireframe mode
+    // ----------------------
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     
     /***** render loop *****/
     while(!glfwWindowShouldClose(window))
@@ -139,7 +153,7 @@ int main()
         // draw
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0);
         // glBindVertexArray(0); // no need to unbind it every time 
 
         glfwSwapBuffers(window);
@@ -150,6 +164,7 @@ int main()
     // -------------------------------------------------------------
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
 
     /***** clean *****/
