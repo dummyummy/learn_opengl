3afae984a95f980c145d0b14dac437d0f7776d12
dummyummy <3384447470@qq.com>
2025-01-06 14:14:28 +0800

    more texture units!

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 #include "shader.h"
 #include "config.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "std_image.h"
+
 #include <iostream>
 #include <cmath>
 
@@ -61,14 +64,15 @@ int main()
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     float vertices[] = {
-        // 位置              // 颜色
-        0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // 右下
-        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // 左下
-        0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // 顶部
+        // positions          // colors           // texture coords
+         0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 1.0f,   2.0f, 2.0f, // top right
+         0.5f, -0.5f, 0.0f,   1.0f, 1.0f, 1.0f,   2.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 1.0f,   0.0f, 2.0f  // top left 
     };
-
-    unsigned int indices[] = {
-        0, 1, 2, // 第一个三角形
+    unsigned int indices[] = {  
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
     };
 
     unsigned int VBO, VAO, EBO;
@@ -83,10 +87,12 @@ int main()
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     // enable the first attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
     glBindVertexArray(0); // unbind VAO
 
@@ -96,6 +102,57 @@ int main()
     // draw in wireframe mode
     // ----------------------
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    // load and create texture
+    unsigned int texture1, texture2;
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    stbi_set_flip_vertically_on_load(true);
+
+    int width, height, nrChannels;
+    const char *texture_path = CMAKE_SOURCE_DIR"/resources/textures/container.jpg";
+    unsigned char *data = stbi_load(texture_path, &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cerr << "Failed to load texture1 " << texture_path << std::endl;
+    }
+    stbi_image_free(data);
+
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    texture_path = CMAKE_SOURCE_DIR"/resources/textures/awesomeface.png";
+    data = stbi_load(texture_path, &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cerr << "Failed to load texture2 " << texture_path << std::endl;
+    }
+    stbi_image_free(data);
+
+    ourShader.use();
+    ourShader.setInt("texture1", 0);
+    ourShader.setInt("texture2", 1);
     
     /***** render loop *****/
     while(!glfwWindowShouldClose(window))
@@ -110,21 +167,15 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
 
         // draw
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+
         ourShader.use();
-
-        // 更新uniform颜色
-        float timeValue = glfwGetTime();
-        float x = timeValue - floorf(timeValue / 3) * 3;
-        float time1 = x < 1 ? 1 - x : (x > 2 ? x - 2 : 0);
-        float time2 = x < 2 ? 1 - fabsf(x - 1) : 0;
-        float time3 = x > 1 ? 1 - fabsf(x - 2) : 0;
-        ourShader.setFloat("time1", time1);
-        ourShader.setFloat("time2", time2);
-        ourShader.setFloat("time3", time3);
-
         glBindVertexArray(VAO);
-        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0);
+        // glDrawArrays(GL_TRIANGLES, 0, 3);
         // glBindVertexArray(0); // no need to unbind it every time 
 
         glfwSwapBuffers(window);
--- shaders/vert.glsl
+++ shaders/vert.glsl
@@ -1,17 +1,14 @@
 #version 330 core
-layout (location = 0) in vec3 aPos;   // 位置变量的属性位置值为 0 
-layout (location = 1) in vec3 aColor; // 颜色变量的属性位置值为 1
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aTexCoord;
 
-uniform float time1;
-uniform float time2;
-uniform float time3;
 out vec3 ourColor; // 向片段着色器输出一个颜色
+out vec2 texCoord;
 
 void main()
 {
     gl_Position = vec4(aPos, 1.0);
-    vec3 rv = vec3(time1, time2, time3);
-    vec3 gv = vec3(time2, time3, time1);
-    vec3 bv = vec3(time3, time1, time2);
-    ourColor = aColor.r * rv + aColor.g * gv + aColor.b * bv; // 将ourColor设置为我们从顶点数据那里得到的输入颜色
+    ourColor = aColor; // 将ourColor设置为我们从顶点数据那里得到的输入颜色
+    texCoord = aTexCoord;
 }
